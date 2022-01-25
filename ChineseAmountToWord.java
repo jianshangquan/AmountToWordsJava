25fd2cd685fef11554bfc17ff6dab19a9a3a4c3c
简尚全 <jianshangquan3@gmail.com>
2022-01-25 13:09:08 +0630

    add english and chinese

--- ChineseAmountToWord.java
+++ ChineseAmountToWord.java
@@ -3,6 +3,9 @@ package com.jiantech.AmountToWords;
 import java.math.BigDecimal;
 
 public class ChineseAmountToWord extends AmountToWord{
+    public static final String[] unit = {"", "十", "百", "千", "万"};
+    public static final String[] numbers = {"一", "二", "三", "四", "五", "六", "七", "八", "九", "零"};
+
     ChineseAmountToWord(){
 
     }
--- EnglishAmountToWord.java
+++ EnglishAmountToWord.java
@@ -1,8 +1,31 @@
 package com.jiantech.AmountToWords;
 
-import java.math.BigDecimal;
 
 public class EnglishAmountToWord extends AmountToWord{
+    public static final String[] unit = {"", "", "Hundred", "Thousand"};
+    public static final String[] twentiesUnit = {
+            "",
+            "Eleven",
+            "Twelve",
+            "Thirteen",
+            "Fourteen",
+            "Sixteen",
+            "Seventeen",
+            "Eighteen",
+            "Nineteen"
+    };
+    public static final String[] tenUnits = {
+            "",
+            "Twenty",
+            "Thirty",
+            "Forty",
+            "Fifty",
+            "Sixty",
+            "Seventy",
+            "Eighty",
+            "Ninety"
+    };
+    public static final String[] numbers = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Zero"};
 
     @Override
     String convert() {
