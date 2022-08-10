1b25ade2685bdc14c34bdfe74fbbd0736a641ebf
jianshangquan <jianshangquan3@gmail.com>
2022-08-10 17:40:24 +0630

    v0.0.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String result = AmountToWord.convertTo(new BigDecimal("1234567891011"), AmountConversion.BURMESE);
+        String result = AmountToWord.convertTo(new BigDecimal("123456789101"), AmountConversion.BURMESE);
         System.out.println(result);
     }
 }
